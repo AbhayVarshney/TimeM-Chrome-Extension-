dcde08c77da37ac9abc1ee4f1784f178cd4f9295
VarunAgarwalDev <varun.agarwal@gmail.com>
2014-11-09 17:32:20 -0800

    history.js fix

--- history.js
+++ history.js
@@ -2,14 +2,6 @@ chrome.history.onVisited.addListener(function(result){
 	alert(result);
 });
 
-/*
-chrome.tabs.query({active : true,
-	url: "*.google.com*"
-}, function(){
-	alert("google tab is active");
-}
-	);*/
-
 var callback = function(historyItem){
 	console.log(historyItem);
 	alert(historyItem);
