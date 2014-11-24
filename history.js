90828a97458d12028f8633e463f018d2d508f393
VarunAgarwalDev <varun.agarwal@gmail.com>
2014-11-23 17:45:13 -0800

    attempting page redder

--- history.js
+++ history.js
@@ -3,11 +3,11 @@ chrome.history.onVisited.addListener(function(result){
 	alert(result);
 });
 */
-
-console.log('Turning ' + tab.url + ' red!');
-chrome.tabs.executeScript({
-    code: 'document.body.style.backgroundColor="red"'
-  });
+chrome.browserAction.onClicked.addListener(function(tab) {
+	console.log('Turning ' + tab.url + ' red!');
+	chrome.tabs.executeScript({
+	    code: 'document.body.style.backgroundColor="red"'
+	  });
 });
 
 
