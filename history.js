4205dc83796e7988078b78ee4023b3143085341c
VarunAgarwalDev <varun.agarwal@gmail.com>
2014-11-23 17:36:53 -0800

    fixed

--- history.js
+++ history.js
@@ -2,12 +2,5 @@ chrome.history.onVisited.addListener(function(result){
 	alert(result);
 });
 
-var callback = function(historyItem){
-	console.log(historyItem);
-	alert(historyItem);
-}
 
-chrome.history.onVisited.addListener(function(result){
-	alert(result);
-})
 
