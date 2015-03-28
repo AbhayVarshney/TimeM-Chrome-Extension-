5980d675317adcc3f056b96a6ccee159e02911ee
VarunAgarwalDev <varun.agarwal@gmail.com>
2015-03-28 12:54:31 -0700

    removing useless files

--- mypopup.js
+++ mypopup.js
@@ -1,83 +1,29 @@
-chrome.tabs.getSelected(null, function(tab) {
-    document.getElementById('currentLink').innerHTML = tab.url;
-});
-
-
-
-
-
-/*$(function() {
-
-	var BG = chrome.extension.getBackgroundPage();
-
-
-	$('.clickme').ready(function(){
-		$('body').on('click', 'a', function(){
-			chrome.tabs.create({url: $(this).attr('href')});
-			return false;
-			console.log("test");
-		});
-	});
-
-})
-*/
-
-//PARSE CODE NOT WORKING
-
-// Authenticating User to allow user to enter the assignments.html page, logged in
 $(document).ready(function(){
-Parse.initialize("LcQYRvseB9ExXGIherTt1v2pw2MVzPFwVXfigo11", "F5enB5XfOfqo4ReAItZCkJVxOY76hoveZrOMwih9");
-// 	var Mailgun = require('mailgun');
-// 	Mailgun.initialize('sandbox1f23a7865ec340f89b5754c264dd17a4.mailgun.org', 'key-269c42f2e1271f965eb92622c2c5101a');
-/*
- 	$("#contactUsButton").on('click', function(e){
-		var name = $("#fullNameField").val();
- 		var userEmail = $("#userEmailField").val();
- 		var comments = $("commentsField").val();
-		
-		console.log("Name: " + name);
+	//Intializes Parse
+	Parse.initialize("LcQYRvseB9ExXGIherTt1v2pw2MVzPFwVXfigo11", "F5enB5XfOfqo4ReAItZCkJVxOY76hoveZrOMwih9");
 
-	Mailgun.sendEmail({
- 			to: "varshneyabhay@gmail.com",
-			from: userEmail,
- 			subject: "Email from " + name,
- 			text: comments
- 		}, {
- 			success: function(httpResponse) {
-				console.log(httpResponse);
- 				response.success("Email sent!");
-			},
-			error: function(httpResponse) {
-				console.error(httpResponse);
- 				response.error("Uh oh, something went wrong");
- 			}
- 		});
-
-})
-*/
-
-
-//Gathers username and password
-function signIn(){
+	//Function is called when sign in button is clicked
+	$("#signIn").on('click', function(e){
 		e.preventDefault();
 		//stores values of username and password
 		var username = $("#username").val();
 		var password = $("#password").val();
 		
+		//Checks to make sure that there is a valid length for the entries
 		if(username.length < 0){
 		  alert("Please type in your username"); 
 		}
 		if(password.length < 0){
 		  alert("Please type in your password"); 
 		}
+
 		authenticateUser(username, password);
 	});
 
-//Validates user
-	/* Adding Username */
-	var makeAssignment,getAssignment;
+
+	//Validates user
 	//function to find an existing user
-	var authenticateUser = function(username, password){
+	function authenticateUser(username, password){
 		Parse.User.logIn(username, password, {
 			success: function(user) {
 			// Do stuff after successful login.
@@ -91,19 +37,10 @@ function signIn(){
 		});
 	};
 
-// 	var newUser = function (username, password){
-// 		var user = new Parse.User();
-// 		user.set("username", username);
-// 		user.set("password", password);
-// 		user.signUp(null, {
-// 			success: function(user) {
-// 				// Hooray! Let them use the app now.
-// 				alert("new user has been created", user.get("username"));
-// 			}, error: function(user, error) {
-// 				// Show the error message somewhere and let the user try again.
-// 				alert("Error: " + error.code + " " + error.message);
-// 			}
-// 		});
-// 	};
-// });
 
+//This will gather the link of the current tab
+	chrome.tabs.getSelected(null, function(tab) {
+		document.getElementById('currentLink').innerHTML = tab.url;
+	});
+
+});
